9d2bcaa230a47a09ff40b982bcc986cf68e210b1
Anthony Lukach <anthonylukach@gmail.com>
2012-10-09 23:57:07 -0700

    Small corrections

--- pingotron/record/admin.py
+++ pingotron/record/admin.py
@@ -1,9 +1,5 @@
 from pingotron.record.models import *
 from django.contrib import admin
 
-class GameAdmin(admin.ModelAdmin):
-    inlines = [PlayerScoreInline]
-
 admin.site.register(Player)
-#admin.site.register(Game)
-admin.site.register(Game, GameAdmin)
+admin.site.register(Game)
